3181cc65c9398f672b636a2efd247fa1c546c8a9
aKentorp <43035590+aKentorp@users.noreply.github.com>
2020-02-07 10:54:58 +0100

    first exercise done

---
+++ 1._Intro_Files/1._Exercises/1._Basics.js
@@ -0,0 +1,27 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and variables
+
+var firstName = "Anders";
+var lastName = "Latif";
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+
+console.log("My first name is", firstName, "and my last name is", lastName);
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+var year = "2019";
+var number = 1;
+
+// Add the year plus the number
+// The result should be 2020
+// You cannot touch line 1 or 2
+
+
+console.log(Number(year) + number);
+
+// --------------------------------------
---
+++ 1._Intro_Files/firstFile.js
@@ -0,0 +1,15 @@
+console.log("Det virker");
+
+var color = "red";
+var bil = {type: "Fiat", color};
+
+console.log(bil.color);
+
+var name = "Jakub";
+
+console.log("hello", name);
+
+age = 123;
+
+console.log(name, "is", age, "old");
+
